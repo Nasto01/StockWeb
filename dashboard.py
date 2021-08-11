2ed81e6e00745ff87040ed90af16d08c93657087
Nasto01 <marek.naster@gmail.com>
2021-08-11 17:30:06 +0200

    removed matplotlib

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests as requests
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #import tweepy
 
 from alphavantage import *
